026607c7243961e0db9a86ecb7e86388085c7942
Luke Mossbarger <mossbargerphone@gmail.com>
2026-01-08 16:02:11 -0600

    first step

--- app/main.go
+++ app/main.go
@@ -17,15 +17,15 @@ func main() {
 
 	// Uncomment this block to pass the first stage!
 	//
-	// command := os.Args[3]
-	// args := os.Args[4:len(os.Args)]
-	//
-	// cmd := exec.Command(command, args...)
-	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// fmt.Println(string(output))
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+	
+	cmd := exec.Command(command, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+	
+	fmt.Println(string(output))
 }
