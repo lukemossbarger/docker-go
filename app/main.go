ba7606e873d27ec537b6f1fb8959cf4ee45da798
Luke Mossbarger <mossbargerphone@gmail.com>
2026-01-08 16:21:03 -0600

    second stage fix

--- app/main.go
+++ app/main.go
@@ -13,7 +13,7 @@ var _ = exec.Command
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	//fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
 	//
@@ -30,5 +30,5 @@ func main() {
 		os.Exit(1)
 	}
 	
-	fmt.Println(string(output))
+	//fmt.Println(string(output))
 }
